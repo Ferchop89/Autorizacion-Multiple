d91b53aafb6e1c2a284cd596e21976953bd3d718
Ferchop89 <fechopacheco@gmail.com>
2018-04-13 13:03:10 -0500

    Leccion 6

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,11 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Route::macro('catch', function($action){
+            $this->any('{anything}', $action)
+                ->where('anything', '.*')
+                ->fallback();
+        });
 
         parent::boot();
     }
--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,7 @@
 <?php
 
+// (1)use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 // Route::get('/', function () {
 //     return view('/admin/dashboard');
 // })->name('admin_dashboard');
@@ -9,3 +11,17 @@ Route::get('/', 'Admin\Dashboard@index')->name('admin_dashboard');
 Route::get('/events', function () {
     return 'Admin Events';
 })->name('admin_events');
+
+Route::post('/events/create', function (){
+    return 'Events created!!';
+});
+
+Route::catch(function(){
+    return response()->view('errors/404', [], 404);
+    // (1)throw new NotFoundHttpException('Pagina no encontrada');
+});
+
+// Route::any('{any}', function(){
+//     return response()->view('errors/404', [], 404);
+//     // (1)throw new NotFoundHttpException('Pagina no encontrada');
+// })->where('any', '.*');
